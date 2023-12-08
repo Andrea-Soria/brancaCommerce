7864028fe034ad76658d6b37788f4faf3cdb0ded
andrea soria <andreasg16@hotmail.com.ar>
2023-12-08 00:13:45 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+import Navbar from "./components/Navbar/Navbar"
+import {ItemListContainer} from "./components/ItemListContainer/ItemListContainer"
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <ItemListContainer greeting={'Bienvenidos'}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+
+
+export const ItemListContainer = ({greeting}) => {
+  return (
+         
+         <p style={{fontSize: '50px', textAlign: 'center'}}> {greeting} </p>
+    
+  )
+}
+
